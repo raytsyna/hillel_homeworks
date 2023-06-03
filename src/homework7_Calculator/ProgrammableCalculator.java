98e27fa9f93ff004de76f014f0ab94919a99805f
raytsyna <raytsyna@gmail.com>
2023-06-03 19:31:07 +0300

    homework9

---
+++ src/homework7_Calculator/ProgrammableCalculator.java
@@ -0,0 +1,9 @@
+package homework7_Calculator;
+
+class ProgrammableCalculator extends Calculator {
+    public double performCustomCalculation(String expression) {
+        double result = 0.0;
+        return result;
+    }
+}
+
